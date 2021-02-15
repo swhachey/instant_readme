18832e158e128f28385717b3355114c8ec198974
swhachey <sw.hachey@gmail.com>
2021-02-14 20:01:27 -0700

    created functions and promises that take your answers to the prompts and add them to a new README file, now i need formatting and licenses

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
+const writeFileAsync = util.promisify(fs.writeFile);
+
 // TODO: Create an array of questions for user input
-// const questions = () => {
-//   return 
-  inquirer.prompt([
+const questions = () => {
+  return inquirer.prompt([
     {
       type: 'input',
       name: 'title',
@@ -38,13 +39,30 @@ const util = require("util");
       message: "Any tests created?",
     },
   ]);
-// };
+};
+
+const generateReadMe = (answers) =>
+`#${answers.title}
+---------------------------
+##${answers.description}
+---------------------------
+###${answers.installation}
+___________________________
+`;
+
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+questions()
+.then((answers) => writeFileAsync("README.md", generateReadMe(answers)))
+.then(()=> console.log("Created your README file"))
+.catch((err) => console.error(err));
+};
 
 // Function call to initialize app
 init();
